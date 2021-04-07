4292485a43a21a73b010f4b92ed2793e6f321a35
VadymBezsmertnyi <vados199368@gmail.com>
2021-04-07 21:45:31 +0300

    Finish Part 5 07.04.2021

--- Base_JS/Part 5/js/script.js
+++ Base_JS/Part 5/js/script.js
@@ -24,3 +24,38 @@ const movieDB = {
     ]
 };
 
+// 1. I working
+let reklama = document.querySelector('.promo__adv');
+//console.log(reklama);
+let reklamaImg = reklama.getElementsByTagName('img');
+//console.log(reklamaImg);
+for (let i = 0; i < reklamaImg.length;) {
+  reklamaImg[i].remove();    
+}
+//console.log(reklamaImg);
+
+// 2.
+let genre = document.querySelector('.promo__genre');
+//console.log(genre);
+//console.log(genre.textContent);
+genre.textContent = 'ДРАМА';
+//console.log(genre.textContent);
+
+// 3.
+let promoBg = document.querySelector('.promo__bg');
+//console.log(promoBg);
+promoBg.style.backgroundImage = 'url("img/bg.jpg")';
+//promoBg.style.backgroundImage = 'img/bg.jpg';
+
+// 4.
+movieDB.movies.sort();
+console.log(movieDB.movies);
+let spisokFilms = document.querySelector('.promo__interactive-list');
+console.log(spisokFilms);
+spisokFilms.innerHTML = "";
+
+movieDB.movies.forEach((film, i) =>{
+    spisokFilms.innerHTML += `<li class="promo__interactive-item">${i + 1} ${film}
+    <div class="delete"></div>
+    </li>`;
+});
