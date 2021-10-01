17c3289c74290bbf5f169c4228a98e2cefb14926
Clyga_Xaocy <vados199368@gmail.com>
2021-10-01 11:00:09 +0300

    Add Testing Area

---
+++ testing-react/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function WhoAmI({name, surname, link}) {
+	return (
+		<div>
+			<h1>My name is {name}, surname - {surname}</h1>
+			<a href={link}>My profile</a>
+		</div>
+	)
+}
+
+function App() {
+	return (
+		<div className="App">
+			<WhoAmI name="John" surname="Smith" link="facebook.com" />
+			<WhoAmI name="Alex" surname="Shepard" link="vk.com" />
+		</div>
+	);
+}
+
+export default App;
