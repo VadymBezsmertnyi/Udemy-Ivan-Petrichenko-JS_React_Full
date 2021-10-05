cf1b8081bfb95f9b9480c8bbe597771ebeb51e55
VadymBezsmertnyi <vados199368@gmail.com>
2021-10-05 15:19:44 +0300

    Learn React

--- testing-react/src/App.js
+++ testing-react/src/App.js
@@ -1,19 +1,54 @@
+import { Component } from 'react';
+
 import './App.css';
 
-function WhoAmI({name, surname, link}) {
-	return (
-		<div>
-			<h1>My name is {name.firstname}, surname - {surname}</h1>
-			<a href={link}>My profile</a>
-		</div>
-	)
+class WhoAmI extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			years: 27,
+			position: ''
+		}
+	}
+
+	nextYear = () => {
+		this.setState(state => ({
+			years: state.years + 1
+		}))
+	}
+
+	connitInputChanges = (e, color) => {
+		console.log(color);
+		this.setState({
+			position: e.target.value
+		})
+	}
+
+	render() {
+		const { name, surname, link } = this.props;
+		const { position, years } = this.state;
+
+		return (
+			<div>
+				<button onClick={this.nextYear}>+++</button>
+				<h1>My name is {name}, surname - {surname}, 
+				age - {years}, 
+				position - {position}</h1>
+				<a href={link}>My profile</a>
+				<form>
+					<span>Введите должность</span>
+					<input type="text" onChange={(e) =>this.connitInputChanges(e,'some color')} />
+				</form>
+			</div>
+		)
+	}
 }
 
 function App() {
 	return (
 		<div className="App">
-			<WhoAmI name={() => {return'John'}} surname="Smith" link="facebook.com" />
-			<WhoAmI name={() => {return'John'}} surname="Shepard" link="vk.com" />
+			<WhoAmI name='John' surname="Smith" link="facebook.com" />
+			<WhoAmI name='Alex' surname="Shepard" link="vk.com" />
 		</div>
 	);
 }
