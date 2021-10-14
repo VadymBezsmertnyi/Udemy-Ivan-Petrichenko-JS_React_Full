38911d9c4706928e9f981a2df4d125afe0615a7f
VadymBezsmertnyi <vados199368@gmail.com>
2021-10-14 18:17:35 +0300

    Practical: add fix Error messenger

--- ReactJS/middle_React/src/components/app/App.js
+++ ReactJS/middle_React/src/components/app/App.js
@@ -4,6 +4,7 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -13,23 +14,28 @@ class App extends Component {
         selectedChar: null
     }
 
-    onCharSelected = (id) =>{
+    onCharSelected = (id) => {
         this.setState({
-            selectedChar:id
+            selectedChar: id
         })
     }
 
     render() {
         return (
             <div className="app">
-                <AppHeader/>
+                <AppHeader />
                 <main>
-                    <RandomChar/>
+                    <ErrorBoundary>
+                        <RandomChar />
+                    </ErrorBoundary>
                     <div className="char__content">
-                        <CharList onCharSelected={this.onCharSelected}/>
-                        <CharInfo charId={this.state.selectedChar}/>
+                        <ErrorBoundary>
+                            <CharList onCharSelected={this.onCharSelected} /></ErrorBoundary>
+                        <ErrorBoundary>
+                            <CharInfo charId={this.state.selectedChar} />
+                        </ErrorBoundary>
                     </div>
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                    <img className="bg-decoration" src={decoration} alt="vision" />
                 </main>
             </div>
         )
